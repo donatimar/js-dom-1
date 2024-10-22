ba33552ac5695627fe3d75eae2f067d8db5e1812
donatimar <donatimar@gmail.com>
2024-10-22 14:09:28 +0200

    added switch lamp with js and modified button for better aesthetics

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-// * Seleziono gli elementi
+// * creo le variabili
 const lamp = document.getElementById("lamp");
 const button = document.getElementById("toggle-button");
 
-// * Variabile per tenere traccia dello stato della lampadina
+// * variabile per tenere traccia dello stato della lampadina
 let lampIsOn = false;
+
+// * event listener per il cambio di stato della lampadina
+button.addEventListener("click", () => {
+  // Controllo se il testo del bottone include "Accendi"
+  if (button.textContent.includes("Accendi")) {
+    // Cambia immagine a lampadina accesa
+    lamp.src = "./img/yellow_lamp.png";
+    // Cambia il testo con "Spegni"
+    button.textContent = "Spegni";
+    console.log("Lampadina accesa");
+  } else {
+    // Cambia immagine a lampadina spenta
+    lamp.src = "./img/white_lamp.png";
+    // Cambia il testo con "Accendi"
+    button.textContent = "Accendi";
+    console.log("Lampadina spenta");
+  }
+});
