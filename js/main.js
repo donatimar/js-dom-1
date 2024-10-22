34fc30438fe97bb971607a8e28fb63cb994f2dd7
donatimar <donatimar@gmail.com>
2024-10-22 13:59:55 +0200

    added variables in js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
+// * Seleziono gli elementi
+const lamp = document.getElementById("lamp");
+const button = document.getElementById("toggle-button");
 
+// * Variabile per tenere traccia dello stato della lampadina
+let lampIsOn = false;
